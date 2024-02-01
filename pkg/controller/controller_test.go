9cbd28a2ad467a185864224a7db6a9714899ec4f
Baptiste Girard-Carrabin <baptiste.girardcarrabin@datadoghq.com>
2024-02-01 17:56:08 +0100

    [local] Add diskLastModificationTime annotation

--- pkg/controller/controller_test.go
+++ pkg/controller/controller_test.go
@@ -279,6 +279,12 @@ func verifyAnnotationsOnPV(updatedAnnotations, expectedAnnotations map[string]st
 			return fmt.Errorf("unexpected annotation on PV: %s (value : %s)", k, v)
 		}
 	}
+	// Begin Datadog patch
+	// We can't check the annotation in the loop before because it is not present on the PVC
+	if _, ok := updatedAnnotations["ebs.csi.aws.com/diskLastModificationTime"]; !ok {
+		return fmt.Errorf("missing diskLastModificationTime annotation on PV (got : %s)", updatedAnnotations)
+	}
+	// End Datadog patch
 	return nil
 }
 
